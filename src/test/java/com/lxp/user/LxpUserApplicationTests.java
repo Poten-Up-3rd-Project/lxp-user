26e3e5d931259d78cd38af65b5794313dfa3f377
dnya0 <annay01794@gmail.com>
2025-12-24 14:37:22 +0900

    chore: initialize project

---
+++ src/test/java/com/lxp/user/LxpUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxp.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxpUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
