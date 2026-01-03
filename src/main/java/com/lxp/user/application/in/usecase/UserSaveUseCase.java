442bcbe225e0ba62af34c309ac798e7c7c862d8a
dnya0 <annay01794@gmail.com>
2026-01-04 00:55:53 +0900

    refactor: return view model instead of domain from read persistence

--- src/main/java/com/lxp/user/application/in/usecase/UserSaveUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.lxp.user.application.in.usecase;
-
-import com.lxp.common.application.port.in.CommandWithResultUseCase;
-import com.lxp.user.application.in.command.UserSaveCommand;
-
-public interface UserSaveUseCase extends CommandWithResultUseCase<UserSaveCommand, Void> {
-}
