351f11df4b600a4f606843bd3141650bd9027c32
dnya0 <annay01794@gmail.com>
2026-01-10 20:32:28 +0900

    feat: add role search api

--- src/main/java/com/lxp/user/infrastructure/web/external/passport/config/PassportConfig.java
+++ src/main/java/com/lxp/user/infrastructure/web/external/passport/config/PassportConfig.java
@@ -3,8 +3,10 @@ package com.lxp.user.infrastructure.web.external.passport.config;
 import com.lxp.user.infrastructure.web.external.passport.filter.PassportAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -15,8 +17,13 @@ public class PassportConfig {
     public SecurityFilterChain filterChain(HttpSecurity http, PassportAuthenticationFilter passportFilter) throws Exception {
         return http
             .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
             .addFilterBefore(passportFilter, UsernamePasswordAuthenticationFilter.class)
-            .authorizeHttpRequests(auth -> auth
+            .authorizeHttpRequests(user -> user
+                .requestMatchers(HttpMethod.POST, "/internal/api-v1/users").permitAll()
+                .requestMatchers("/internal/api-v1/users/*/role").permitAll()
                 .anyRequest().authenticated()
             )
             .build();
--- src/main/java/com/lxp/user/infrastructure/web/internal/controller/UserInternalController.java
+++ src/main/java/com/lxp/user/infrastructure/web/internal/controller/UserInternalController.java
@@ -7,28 +7,49 @@ import com.lxp.user.infrastructure.web.internal.controller.dto.UserInfoResponse;
 import com.lxp.user.infrastructure.web.internal.controller.dto.UserSaveRequest;
 import com.lxp.user.infrastructure.web.internal.controller.mapper.UserInternalMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/internal/api-v1/users")
 @RequiredArgsConstructor
 public class UserInternalController {
 
+    @Value("${internal.auth-token}")
+    private String internalAuthToken;
+
     private final UserSaveUseCase userSaveUseCase;
     private final UserFindInternalUseCase userFindInternalUseCase;
     private final UserInternalMapper userInternalMapper;
 
     @PostMapping
-    public ResponseEntity<Void> register(@RequestBody UserSaveRequest request) {
+    public ResponseEntity<Void> register(@RequestHeader(name = "Authorization") String token,
+                                         @RequestBody UserSaveRequest request) {
+        if (!isValidInternalToken(token)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
         userSaveUseCase.execute(userInternalMapper.toUserSaveCommand(request));
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{userId}/role")
+    public ResponseEntity<String> getRole(@RequestHeader(name = "Authorization") String token,
+                                          @PathVariable String userId) {
+        if (!isValidInternalToken(token)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+        return ResponseEntity.ok(userFindInternalUseCase.execute(userId).role());
     }
 
     @GetMapping
@@ -36,6 +57,14 @@ public class UserInternalController {
         UserInfoInternalResult execute = userFindInternalUseCase.execute(userId);
         userInternalMapper.toUserInfoResponse(execute);
         return ResponseEntity.ok(userInternalMapper.toUserInfoResponse(execute));
+    }
+
+    private boolean isValidInternalToken(String token) {
+        if (Objects.isNull(token) || !token.startsWith("Bearer ")) {
+            return false;
+        }
+
+        return token.substring(7).equals(internalAuthToken);
     }
 
 }
