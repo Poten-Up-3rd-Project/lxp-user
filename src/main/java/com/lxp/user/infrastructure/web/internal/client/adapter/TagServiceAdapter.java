3dbdecc7ecdaa255bbf0c953c9723db6bd9c89c2
dnya0 <annay01794@gmail.com>
2026-01-16 09:20:15 +0900

    feat: connect actual Tag Service API instead of mock data

--- src/main/java/com/lxp/user/infrastructure/web/internal/client/adapter/TagServiceAdapter.java
+++ src/main/java/com/lxp/user/infrastructure/web/internal/client/adapter/TagServiceAdapter.java
@@ -5,20 +5,14 @@ import com.lxp.user.application.port.required.query.TagResult;
 import com.lxp.user.domain.common.exception.UserErrorCode;
 import com.lxp.user.domain.common.exception.UserException;
 import com.lxp.user.infrastructure.web.internal.client.TagServiceFeignClient;
-import com.lxp.user.infrastructure.web.internal.client.dto.TagInfo;
-import com.lxp.user.infrastructure.web.internal.client.dto.TagListResponse;
+import com.lxp.user.infrastructure.web.internal.client.support.ResponseAssertions;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-/**
- * TODO
- *      tag service가 만들어지면 수정할 것
- */
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -29,19 +23,7 @@ public class TagServiceAdapter implements TagServicePort {
     @Override
     @CircuitBreaker(name = "tagService", fallbackMethod = "findTagsFallback")
     public List<TagResult> findTags(List<Long> ids) {
-        //        TagListResponse tagListResponse = ResponseAssertions.getBodyIf2xx(tagServiceFeignClient.findTags(ids));
-
-        //임시 값
-        List<TagInfo> collect = ids.stream().map(id ->
-            new TagInfo(id, "test" + id, "color" + id, "variant" + id)
-        ).collect(Collectors.toList());
-
-        TagListResponse tagListResponse = new TagListResponse(collect);
-
-        List<TagResult> tagResults = tagListResponse.tags().stream().map(tag ->
-            new TagResult(tag.id(), tag.name(), tag.color(), tag.variant())
-        ).toList();
-
+        List<TagResult> tagResults = ResponseAssertions.getBodyIf2xx(tagServiceFeignClient.findTags(ids));
         log.info("find tags success, ids={}, tags={}", ids, tagResults);
         return tagResults;
     }
