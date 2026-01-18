3b8eb61ca3f5270bdcd0b55a06ecbfe42e752a15
dnya0 <annay01794@gmail.com>
2026-01-18 21:05:45 +0900

    refactor: modify resolve DeliveryPolicy

--- src/main/java/com/lxp/user/application/event/policy/DeliveryPolicyResolver.java
+++ src/main/java/com/lxp/user/application/event/policy/DeliveryPolicyResolver.java
@@ -10,13 +10,16 @@ public class DeliveryPolicyResolver {
      * Domain Event에 따라 전달 정책 결정
      * 핵심 이벤트(user.created, user.updated, user.deleted)는 OUTBOX_REQUIRED
      * 기타 이벤트는 FIRE_AND_FORGET
+     *
+     * todo recommend 완료되면 수정할 것
      */
     public DeliveryPolicy resolve(BaseDomainEvent event) {
-        String eventType = event.getClass().getSimpleName();
-        
-        return switch (eventType) {
-            case "UserCreatedEvent", "UserUpdatedEvent", "UserWithdrawEvent" -> DeliveryPolicy.OUTBOX_REQUIRED;
-            default -> DeliveryPolicy.FIRE_AND_FORGET;
-        };
+        return DeliveryPolicy.FIRE_AND_FORGET;
+//        String eventType = event.getClass().getSimpleName();
+//
+//        return switch (eventType) {
+//            case "UserCreatedEvent", "UserUpdatedEvent", "UserWithdrawEvent" -> DeliveryPolicy.OUTBOX_REQUIRED;
+//            default -> DeliveryPolicy.FIRE_AND_FORGET;
+//        };
     }
 }
